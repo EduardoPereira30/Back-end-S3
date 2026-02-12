e171a47de718f8a2e625c43e833f3dc3e331a73b
EDUARDO PEREIRA SILVA <41932003886@info.local>
2026-02-12 10:46:59 -0300

    Revição

--- revição-POO/ex3/obj/Debug/net10.0/ex3.AssemblyInfo.cs
+++ revição-POO/ex3/obj/Debug/net10.0/ex3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ex1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ex3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d676e777ee5bd3cf0969ac782b11048c3470b2")]
-[assembly: System.Reflection.AssemblyProductAttribute("ex1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ex1")]
+[assembly: System.Reflection.AssemblyProductAttribute("ex3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ex3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
